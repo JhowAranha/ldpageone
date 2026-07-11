d1a9c07bf62dd1136c0cc1399863615b88357ac0
JhowAranha <jhonatan.aranha@hotmail.com>
2026-07-10 23:18:50 -0300

    alter sitemap url again

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://shiftflow.com', // URL da sua Home
+      url: 'https://ldpageone.vercel.app', // URL da sua Home
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
